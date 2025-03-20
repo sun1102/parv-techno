3260ccb8c00b733892a9fcd08a7749b491f63c45
Sunil Dadhich <sunil.dadhich2010@hotmail.com>
2025-03-20 10:00:16 +0530

    routing fix

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/index" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/projects" element={<Projects />} />
